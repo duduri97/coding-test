bc922249163b1c663ee9ffe6063c51916a59513f
duduri97 <ginamkim97@gmail.com>
2025-12-29 07:20:26 +0900

    solve: 계수 정렬(Counting Sort)을 이용한 수 정렬 구현

---
+++ 01.기초/counting_sort.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+count = [0] * 1001
+numbers = list(map(int, sys.stdin.readline().split()))
+
+for number in numbers:
+    count[number] += 1
+
+for i in range(1001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            sys.stdout.write(str(i) + ' ')
